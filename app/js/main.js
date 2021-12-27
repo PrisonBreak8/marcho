c0872293a3902bf937437d3ee4c564389f5278c8
PrisonBreak8 <golubev.artur.1986@gmail.com>
2021-12-27 17:32:03 +0300

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,40 @@
 $(function () {
+
+
+	//SHOP CONTENT FILTER ===============================================
+	$('.shop-content__filter-btn').on('click', function () {
+		$('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+		$(this).addClass('shop-content__filter-btn--active');
+	});
+
+	$('.button-list').on('click', function () {
+		$('.product-item').addClass('product-item--list');
+	});
+
+	$('.button-grid').on('click', function () {
+		$('.product-item').removeClass('product-item--list');
+	});
+
+	//https://www.npmjs.com/package/jquery-form-styler   jquery-form-styler===============================================
+	$('.select-style').styler();
+
+
+	//RANGE SLIDER JS ===============================================
+	$('.filter-price__input').ionRangeSlider({
+		type: "double",
+		prefix: "$",
+		onStart: function (data) {
+			$('.filter-price__from').text(data.from);
+			$('.filter-price__to').text(data.to);
+		},
+
+		onChange: function (data) {
+			$('.filter-price__from').text(data.from);
+			$('.filter-price__to').text(data.to);
+		},
+	});
+
+
 	// SLIDER =================================
 	$('.top-slider__inner').slick({
 		dots: true,
